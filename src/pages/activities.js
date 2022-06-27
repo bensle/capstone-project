ed2e7eccf1cacc2cdefd579f59c21b69f9a753fa
Benjamin Scheffler <scheffler-benjamin@web.de>
2022-06-27 16:44:45 +0200

    Change git config ignorecase

---
+++ src/pages/activities.js
@@ -0,0 +1,11 @@
+import ActivityCard from '../components/ActivityCard/ActivityCard';
+
+export default function Activities() {
+  return (
+    <>
+      <h1>Activities</h1>
+
+      <ActivityCard />
+    </>
+  );
+}
