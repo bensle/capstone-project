e2725767bf154d8c365d074c24ee4d75a37329ec
Benjamin Scheffler <scheffler-benjamin@web.de>
2022-07-12 14:12:34 +0200

    Add Info to Fav Page

--- src/pages/FavoritesPage.js
+++ src/pages/FavoritesPage.js
@@ -1,3 +1,7 @@
+import {MdWbSunny} from 'react-icons/md';
+import {MdDeleteForever} from 'react-icons/md';
+import styled from 'styled-components';
+
 import Favorite from '../components/Favorite/Favorite';
 
 export default function FavoritesPage({
@@ -17,7 +21,17 @@ export default function FavoritesPage({
     <div>
       <h2>Favorites</h2>
       <button onClick={onSetIsHidden}>go back</button>
-
+      <StyledP>
+        See your favorite Activities here. To delete them directly click on the{' '}
+        <DelSpan>
+          <MdDeleteForever />
+        </DelSpan>
+        . <br />
+        To remove an activity from your favorites click{' '}
+        <FavSpan>
+          <MdWbSunny />
+        </FavSpan>
+      </StyledP>
       {savedFavorite.map(({id, name, location, duration, type, infos}) => (
         <Favorite
           id={id}
@@ -37,3 +51,19 @@ export default function FavoritesPage({
     </div>
   );
 }
+
+const StyledP = styled.p`
+  border: 2px solid white;
+  border-radius: 6px;
+  background-color: var(--modalbg);
+  padding: 0.2rem;
+  text-align: center;
+  font-size: 0.7rem;
+  margin: 0.5rem 0;
+`;
+const FavSpan = styled.span`
+  color: yellow;
+`;
+const DelSpan = styled.span`
+  color: red;
+`;
